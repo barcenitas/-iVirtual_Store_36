e24aa43e0cb463e5579345e86a33e9389338ac7f
barcenitas <erick3arcenas7@gmail.com>
2018-03-30 11:31:55 -0600

    inicio2

--- s-01-usuarios.sql
+++ s-01-usuarios.sql
@@ -1,1 +1,23 @@
-s-01-usuarios.sql
+--s-01-usuarios.sql
+/*i) Este script contendrá la definición de 2
+usuarios:
+<iniciales>_proy_invitado
+y
+<iniciales>_proy_admin, donde <iniciales>
+corresponden a la primera letra de cada
+apellido paterno de cada integrante. El usuario
+admin será el dueño de todos los objetos del
+caso de estudio.
+ii) Incluir en el script la definición de 2 roles:(1) rol_admin Contendrá todos los roles
+necesarios para poder implementar el caso
+de estudio asignado.
+(2) rol_invitado.
+Solo deberá tener
+permisos para crear sesiones.
+iii) Asignar a los usuarios admin e invitado los
+roles
+rol_admin
+y
+rol_invitado
+respectivamente.*/
+
