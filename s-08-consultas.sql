ca372473821992dfcd76d6dc4673657293db048c
barcenitas <erick3arcenas7@gmail.com>
2018-04-13 13:17:51 -0500

    Nos vemos en la gloria

--- s-08-consultas.sql
+++ s-08-consultas.sql
@@ -6,3 +6,43 @@ relacional y subconsultas (No es necesario que
 una misma consulta haga uso de todos estos
 elementos). Este punto deberá explicarse.
 */
+
+
+---select
+SELECT * FROM Producto;
+SELECT * FROM Pelicula;
+SELECT * FROM AlbumMusical;
+SELECT * FROM VideoJuego;
+SELECT * FROM HistoricoPrecioProducto;
+SELECT * FROM FormaPago;
+SELECT * FROM Cliente;
+SELECT * FROM ModAdquisicion;
+SELECT * FROM Streaming;
+SELECT * FROM Transferencia;
+
+----
+---CONSULTAS
+----
+SELECT * 
+FROM FormaPago
+INNER JOIN Cliente
+ON FormaPago.id_formaPago = Cliente.id_cliente;
+
+----
+SELECT * 
+FROM FormaPago
+RIGHT JOIN Cliente
+ON FormaPago.id_formaPago = Cliente.id_cliente;
+----
+SELECT * 
+FROM FormaPago
+LEFT JOIN Cliente
+ON FormaPago.id_formaPago = Cliente.id_cliente;
+-----
+---SUBCONSULTA FROM
+-----
+
+SELECT nomBanco AS nb
+FROM (SELECT * FROM FormaPago NATURAL JOIN TarCredito) 
+GROUP BY formap
+HAVING COUNT(*)>3;
